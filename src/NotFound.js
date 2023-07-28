cda807895566664d503535746add965b3fb4f9c2
Devanshi <devanshityagi3@gmail.com>
2023-07-28 12:54:35 +0530

    dojo blog completed

---
+++ src/NotFound.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return ( 
+        <div className="not-found">
+            <h1>Not Found!</h1>
+                <img src="notfound.avif" alt="404" />
+        </div>
+     );
+}
+ 
+export default NotFound;
